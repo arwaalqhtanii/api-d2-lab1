24e479b911de5e936ae8f47bc4937a625e200b30
arwaalqhtanii <alqhtaniarwa414@gmail.com>
2024-09-16 10:49:02 +0300

    frist code

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 
- fetch('https://fakestoreapi.com/products', {
-    cache: 'no-cache', 
+
+fetch('https://fakestoreapi.com/products', {
+    cache: 'no-cache',
     headers: {
-        'Cache-Control': 'no-cache' 
+        'Cache-Control': 'no-cache'
     }
 })
 .then(response => response.json())
 .then(products => {
-
     products.sort((a, b) => a.price - b.price);
 
     const container = document.getElementById('products-container');
@@ -15,16 +15,19 @@
     products.forEach(product => {
       
         const productElement = document.createElement('div');
-        productElement.className = 'product';
+        productElement.className = 'col-sm-6 col-md-4 col-lg-3 mb-4'; // Responsive Bootstrap columns
 
         productElement.innerHTML = `
-            <img src="${product.image}" alt="${product.title}">
-            <h2>${product.title}</h2>
-            <p>Price: $${product.price}</p>
-          
+            <div class="card h-100">
+                <img src="${product.image}" class="card-img-top" alt="${product.title}">
+                <div class="card-body d-flex flex-column">
+                    <h5 class="card-title">${product.title}</h5>
+                    <p class="card-text text-primary">Price: $${product.price}</p>
+                </div>
+            </div>
         `;
 
-      
+    
         container.appendChild(productElement);
     });
 });
